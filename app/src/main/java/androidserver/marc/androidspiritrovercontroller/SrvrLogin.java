27a3d5f5d5a5661bf6da17342c8eab68fd1056c4
MarcKarasek <marckarasek@gmail.com>
2018-01-07 15:45:20 -0500

    Initial commit - This will be flleshed out with code from the 32x32 led android client app

---
+++ app/src/main/java/androidserver/marc/androidspiritrovercontroller/SrvrLogin.java
@@ -0,0 +1,13 @@
+package androidserver.marc.androidspiritrovercontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SrvrLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_srvr_login);
+    }
+}
